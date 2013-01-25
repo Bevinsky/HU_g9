a53494067af2c85f63bdb827ce4dbaabb760ae5e
oskar <lundh123@gmail.com>
2013-01-25 02:26:33 +0100

    Update DatabaseRetriever.py

--- DatabaseRetriever.py
+++ DatabaseRetriever.py
@@ -1,13 +1,6 @@
 import MySQLdb as mdb
-##http://phpmyadmin315.loopia.se/
-##
-##anv:kthstud@a68445
-##
-##losen:2013IIstud!#
-##
-##(host='mysql315.loopia.se', user='username', passwd='some pwd', db='aktivahuset_com')
 
-conn=mdb.connect("mysql315.loopia.se","kthstud@a68445","2013IIstud!#","aktivahuset_com")
+conn=mdb.connect()
 with conn:
     cur=conn.cursor()
     cur.execute("SELECT * FROM *")
