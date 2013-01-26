57845b64fe58c7ea8f6767edc42b2a431a0ad4e6
Vin <bevin@jabana.se>
2013-01-26 16:46:45 +0100

    reset temperatureread again

--- TemperatureRead.py
+++ TemperatureRead.py
@@ -1,35 +1,22 @@
 import matplotlib.pyplot as plt
-import csv
 
-def run():
-    fileName="data/stockholm_timmedel.csv"
-    data = []
-    with open(fileName, 'rb') as file:
-        csvread = csv.reader(file, delimiter=';')
-        i = 0;
-        for line in csvread:
-            if i > 30:
-                break
-            print line
-            i += 1
-    
-    quit()
-    
-    data=open(fileName,"r")
-    usefulData=list()
-    for line in data.readlines():
-        x=line.split(";")
-        if "2012" in x[0] or "2013" in x[0]:
-            usefulData.append(x)
-    print usefulData
-    ##axis=list()
-    ##values=list()
-    ##for event in x:
-    ##    axis.append(x[1][:x[1].index(":")])
-    ##    values.append(x[2].replace(",","."))
-    ##
-    ##print axis
-    ##    
-    ##plt.plot(values)
-    ##plt.axis(axis)
-    ##plt.show()
+
+fileName="stockholm_timmedel.csv"
+data=open(fileName,"r")
+usefulData=list()
+for line in data.readlines():
+    x=line.split(";")
+    if "2012" in x[0] or "2013" in x[0]:
+        usefulData.append(x)
+print usefulData
+##axis=list()
+##values=list()
+##for event in x:
+##    axis.append(x[1][:x[1].index(":")])
+##    values.append(x[2].replace(",","."))
+##
+##print axis
+##    
+##plt.plot(values)
+##plt.axis(axis)
+##plt.show()
